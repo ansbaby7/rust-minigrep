a3d03aab6649f7f35274fa1b3679df7e0d563a1e
ansbaby7 <ansbaby7@gmail.com>
2023-01-06 23:04:17 +0530

    Refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
-use std::env;
+use std::{env, fs};
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Config { query, filename }
+    }
+}
+ 
 fn main() {
     let args:Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let config = Config::new(&args);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename).expect("Couldn't read from file");
+    println!("With text:\n{}", contents)
 }
